b04d0532e2129dcda3c34c0f921729901b7e4eca
Felix Kariuki <felka39@gmail.com>
2018-04-14 17:43:21 +0300

    initial comment

---
+++ src/main/java/com/example/felix/android101/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.felix.android101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    String[] drinks = {"Hunters", "Kibao", "KC", "Napoleon", "Tusker", "Whitecap","Johnie Walker", "Jack Daniels", "Jameson","Summit"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, drinks);
+        list.setAdapter(adapter);
+         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 Toast.makeText(MainActivity.this, "Your are drinking "+drinks[position], Toast.LENGTH_SHORT).show();
+             }
+         });
+
+    }
+}
